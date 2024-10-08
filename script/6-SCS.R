639b277552665fff423e84cdfbaa0412ead2f226
zihasyu <1045839067@qq.com>
2024-10-08 16:58:30 +0800

    add SF generation speed log10

--- script/6-SCS.R
+++ script/6-SCS.R
@@ -1,11 +1,13 @@
 library(ggplot2)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # 定义数据
-heights <- c(1, 1, 1,1, 1, 1, 1, 1,1, 1,1, 1, 1,1, 1, 1, 1, 1,1, 1)
+heights <- c(
+191.171,255.963,2774.03,1557.98,6133.12,286.174,381.97,4216.39,1552.11,112726,811.596,1098.75,12005.2,1556.73,48991.9,108.877,145.951,1612.74,1613.42,2956.42
+  )
 
 # 创建数据框
 df1 <- data.frame(
-  group = rep(c('Source','WEB', 'Docker','Log'), each = 5),
+  group = rep(c('Linux', 'Chromium', 'WEB', 'Log'), each = 5),
   category = factor(rep(c('NTrans', 'Finesse', 'Odess', 'Palantir','BiSearch'), times = 4),
                     levels = c('NTrans', 'Finesse', 'Odess', 'Palantir','BiSearch')),
   height = heights
@@ -19,9 +21,15 @@ p1 <- ggplot(df1, aes(x = group, y = height, fill = category)) +
            colour = "black", width = 0.8) + 
   scale_fill_manual(values = c("#f5deb3", "#4a90e2", "#ac8cf4", "#ff7f0e","#FF5733")) +
   labs(y = 'Speed (MiB/s)') +
-  scale_y_continuous(
-    limits = c(0, max_height),
-    expand = expansion(mult = c(0, 0.1))
+  # scale_y_continuous(
+  #   limits = c(0, max_height),
+  #   expand = expansion(mult = c(0, 0.1))
+  # ) +
+  scale_y_log10(
+  limits = c(NA, NA),
+  expand = expansion(mult = c(0, 0.1)),
+  breaks = scales::trans_breaks("log10", function(x) 10^x),
+  labels = scales::trans_format("log10", scales::math_format(10^.x))
   ) +
   theme_bw() +
   theme(
