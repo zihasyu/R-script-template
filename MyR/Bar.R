70c927c5b02f21ca46d482f407d5a1fd9ef5e261
zihasyu <1045839067@qq.com>
2025-04-16 15:43:24 +0800

    updata MyR.R & The sample is complete

--- MyR/Bar.R
+++ MyR/Bar.R
@@ -124,37 +124,3 @@ create_comparison_barplot <- function(
   
   return(p)
 }
-
-
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-data1 <- c(0.211018, 0.473857, 0.031107, 0.178961)
-data2 <- c(0.21847, 0.0581643, 0.0334536, 0.130792)
-labels <- c("Linux", "Web", "Chro.", "Auto.")
-
-# 基本调用
-# plot <- create_comparison_barplot(data1, data2, labels)
-
-# 显示图例的调用
-plot <- create_comparison_barplot(
-  data1 = data1,
-  data2 = data2,
-  labels = labels,
-  fill_names = c("Off. C.", "Mix. Mod."),
-  y_label = "FRP",
-  show_legend = TRUE
-)
-
-# # 完全自定义的调用
-# plot <- create_comparison_barplot(
-#   data1 = c(2.222, 0.6, 0.4),
-#   data2 = c(0.3, 0.5, 0.7),
-#   labels = c("A", "B", "C"),
-#   # fill_colors = c("blue", "red"),
-#   fill_names = c("Original", "Modified"),
-#   x_label = "Categories",
-#   y_label = "Metrics",
-#   export_name = "new_plot.pdf"
-# )
-exportPath="./"
-exportName="bar.pdf"
-ggsave(paste(exportPath, exportName, sep=""), plot = plot, width = 12, height = 6)
--- MyR/MyR.R
+++ MyR/MyR.R
@@ -1,2 +1,11 @@
 source("./Bar.R")
+#create_comparison_barplot 两柱子饼图
 source("./Line_NoPoint.R")
+#这个里面的图都没有点，是一条很曲折的线，适合给性能测试使用
+#plot_line_comparison 每条线上x轴都相等时可以用的
+#plot_line_comparison_xcdf 每条线上x轴不相等时可以用的，x轴会被cdf化
+source("./Line_Point.R")
+#这个里面的图有点，适合给perversion去测一些东西使用
+#plot_line_with_points 显示所有的点
+#plot_line_with_selected_points 只显示10个点，但线条是显示所有点的
+#plot_with_sampled_points 只显示10个点，线条也是显示10个点的
